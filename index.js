77a92fd5b57beef0dcc448f7b929264d21a9529d
levg34 <levg34@users.noreply.github.com>
2017-05-11 10:25:27 +0200

    working on openshift

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var express = require('express')
 var app = express()
+var server = require('http').createServer(app)
 
 app.use(express.static(__dirname + '/public'))
 
@@ -10,6 +11,6 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/view/index.html')
 })
 
-app.listen(server_port, function () {
+server.listen(server_port,server_ip_address,function () {
 	console.log("Listening on " + server_ip_address + ", port " + server_port)
 })
