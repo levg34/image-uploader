07e92f255e6064ec6e835751651408eb7edd933f
levg34 <guilhem.dross@laposte.net>
2017-05-18 11:17:12 +0200

    file upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
+var formidable = require('formidable')
+var fs = require('fs')
 
 app.use(express.static(__dirname + '/public'))
 
@@ -13,6 +15,30 @@ app.get('/', function (req, res) {
 
 app.get('/upload/:nickname', function (req, res) {
 	res.sendFile(__dirname + '/view/upload.html')
+})
+
+app.post('/upload', function (req, res) {
+	var form = new formidable.IncomingForm()
+	console.log('about to parse')
+	form.parse(req, function (error, fields, files) {
+		console.log('parsing done')
+		console.log()
+
+		/* Possible error on Windows systems:
+		 tried to rename to an already existing file */
+		fs.rename(files.upload.path, './public/img/test.png', function (error) {
+			if (error) {
+				fs.unlink('./public/img/test.png')
+				fs.rename(files.upload.path, './public/img/test.png')
+			}
+		});
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		res.write('received image:<br/>')
+		res.write('<img src="/img/test.png"/>')
+		res.end()
+	});
 })
 
 server.listen(server_port,server_ip_address,function () {
